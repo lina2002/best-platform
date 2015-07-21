b5de01657670a65333b6f2f4aa8a59edb1e2a101
lina <lina2002@go2.pl>
2015-07-21 03:31:24 +0200

    Displaying list of courses

--- src/main/java/platform/CoursesController.java
+++ src/main/java/platform/CoursesController.java
@@ -5,13 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class CoursesController {
+    private XMLParser xmlParser = new XMLParser("courses.xml");
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @RequestMapping("/courses")
+    public void courses(Model model) {
+        List<Course> courses = xmlParser.parseXML();
+        model.addAttribute("courses", courses);
     }
 
 }
