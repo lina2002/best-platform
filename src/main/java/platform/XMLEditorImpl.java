1cdd2c9cd3447131f2e2eababd92b70f86724cae
lina <lina2002@go2.pl>
2015-07-22 02:59:06 +0200

    Finished working on XMLEditor implementation

--- src/main/java/platform/XMLEditorImpl.java
+++ src/main/java/platform/XMLEditorImpl.java
@@ -1,14 +1,63 @@
 package platform;
 
-public class XMLEditorImpl implements XMLEditor {
-    private String inputFileName;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-    public XMLEditorImpl(String inputFileName) {
-        this.inputFileName = inputFileName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+
+public class XMLEditorImpl implements XMLEditor {
+    private File file;
+
+    public XMLEditorImpl(String fileName) {
+        this.file = new File(fileName);
     }
 
     @Override
     public void update(Course course) {
-
+        try {
+            tryToUpdate(course);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    private void tryToUpdate(Course course) throws Exception {
+        Document document = getDocument();
+        updateCourseInDocument(course, document);
+        save(document);
+    }
+
+    private Document getDocument() throws Exception {
+        DocumentBuilderFactory documentBuilderFactory =
+                DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder =
+                documentBuilderFactory.newDocumentBuilder();
+        return documentBuilder.parse(file);
+    }
+
+    private void updateCourseInDocument(Course course, Document document) {
+        Node coursesNode = document.getFirstChild();
+        NodeList courses = coursesNode.getChildNodes();
+        Node courseToUpdate = courses.item(2 * course.getId() - 1);
+        NodeList properties = courseToUpdate.getChildNodes();
+        properties.item(1).setTextContent(course.getName());
+        properties.item(3).setTextContent(course.getDescription());
+    }
+
+    private void save(Document document) throws Exception {
+        TransformerFactory transformerFactory =
+                TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(document);
+        StreamResult streamResult = new StreamResult(file);
+        transformer.transform(source, streamResult);
+    }
+
 }
