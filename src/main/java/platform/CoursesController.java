cfb4dfcf2f8e72d81ceaaba778d1ad1cfaadbfcc
patxn <patrycja.nguyen@gmail.com>
2015-07-22 12:20:19 +0200

    Course edit controller

--- src/main/java/platform/CoursesController.java
+++ src/main/java/platform/CoursesController.java
@@ -2,14 +2,17 @@ package platform;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class CoursesController {
-    private XMLParser xmlParser = new XMLParserImpl("courses.xml");
+    private final String inputFileName = "courses.xml";
+    private XMLParser xmlParser = new XMLParserImpl(inputFileName);
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
@@ -28,7 +31,21 @@ public class CoursesController {
         int id = parseId(idFromGet);
         model.addAttribute("course", xmlParser.getCourseByID(id));
     }
-    
+
+    @RequestMapping(value="/edit", method=RequestMethod.GET)
+    public void editForm(@ModelAttribute Course course, Model model) {
+        model.addAttribute("editInformation","");
+        model.addAttribute("course", course);
+    }
+
+    @RequestMapping(value="/edit", method=RequestMethod.POST)
+    public void editSubmit(@ModelAttribute Course course, Model model) {
+        XMLEditor xmlEditor = new XMLEditorImpl(inputFileName);
+        xmlEditor.update(course);
+        model.addAttribute("editInformation","Course has been saved.");
+        model.addAttribute("course", course);
+    }
+
     @RequestMapping("/login")
     public void login(Model model) {
 
